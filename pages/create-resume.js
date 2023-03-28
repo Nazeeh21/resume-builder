64ce6a7fc53c75e93ed0678dda6488e9f1ec87c4
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2023-03-29 02:28:41 +0530

    Add initialState and handlers

---
+++ pages/create-resume.js
@@ -0,0 +1,9 @@
+const CreateResume = () => {
+  return (
+    <div>
+      <h1>Create Resume</h1>
+    </div>
+  )
+}
+
+export default CreateResume
---
+++ warp/contract.js
@@ -0,0 +1,28 @@
+export function handle(state, action) {
+  /* address of the caller is available in action.caller */
+  if (action.input.function === "initialize") {
+    state.author = action.caller;
+  }
+  if (action.input.function === "createResume") {
+    const resumes = state.resumes;
+    const userResume = resumes[action.caller];
+    userResume[action.input.resume.id] = action.input.resume;
+    resumes[action.caller] = userResume;
+    state.resumes = resumes;
+  }
+  if (action.input.function === "updateResume") {
+    const resumes = state.resumes;
+    const userResume = resumes[action.caller];
+    userResume[action.input.resume.id] = action.input.resume;
+    resumes[action.caller] = userResume;
+    state.resumes = resumes;
+  }
+  if (action.input.function === "deleteResume") {
+    const resumes = state.resumes;
+    const userResume = resumes[action.caller];
+    delete userResume[action.input.resume.id];
+    resumes[action.caller] = userResume;
+    state.resumes = resumes;
+  }
+  return { state };
+}
